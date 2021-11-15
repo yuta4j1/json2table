825d4a4539ade64f78eaa80627ae465bb409cec0
yuta4j1 <kascado.ys10@gmail.com>
2021-11-15 23:11:07 +0900

    要素に応じてテーブルの幅が変化するよう修正

--- lib/index.ts
+++ lib/index.ts
@@ -12,24 +12,73 @@ const extractColumns = (dataSrc: any) => {
   return columns;
 };
 
-const createTableHeaderString = (columns: string[]): string => {
+const printTableHeader = (columns: string[], widthArr: number[]) => {
   let str = "|";
-  for (const clmn of columns) {
-    str += ` ${clmn} |`;
+  for (const clidx in columns) {
+    if (columns[clidx].length < widthArr[clidx]) {
+      const maxRowWidth = widthArr[clidx];
+      const emptyLength = maxRowWidth - columns[clidx].length + 2;
+      let preEmptyLen = 0;
+      let postEmptyLen = 0;
+      if (emptyLength % 2 === 0) {
+        preEmptyLen = postEmptyLen = emptyLength / 2;
+      } else {
+        preEmptyLen = emptyLength / 2 + 1;
+        postEmptyLen = emptyLength / 2;
+      }
+
+      str += `${" ".repeat(preEmptyLen)}${columns[clidx]}${" ".repeat(
+        postEmptyLen
+      )}|`;
+    } else {
+      str += ` ${columns[clidx]} |`;
+    }
   }
 
   const ret = `${"_".repeat(str.length)}\n${str}\n${"=".repeat(str.length)}`;
 
-  return ret;
+  console.log(ret);
 };
 
-const createRow = (dataArr: string[]) => {
-  let str = "| ";
-  for (const d of dataArr) {
-    str += ` ${d} |`;
+const printRow = (arr: string[], widthArr: number[]): void => {
+  let str = "|";
+  const dataArr = arr.map((v) => String(v));
+  for (const idx in dataArr) {
+    if (dataArr[idx].length < widthArr[idx]) {
+      const maxRowWidth = widthArr[idx];
+      const emptyLength = maxRowWidth - dataArr[idx].length + 2;
+      let preEmptyLen = 0;
+      let postEmptyLen = 0;
+      if (emptyLength % 2 === 0) {
+        preEmptyLen = postEmptyLen = emptyLength / 2;
+      } else {
+        preEmptyLen = emptyLength / 2 + 1;
+        postEmptyLen = emptyLength / 2;
+      }
+
+      str += `${" ".repeat(preEmptyLen)}${dataArr[idx]}${" ".repeat(
+        postEmptyLen
+      )}|`;
+    } else {
+      str += ` ${dataArr[idx]} |`;
+    }
   }
 
-  return str;
+  console.log(str);
+};
+
+const extractWidth = (clmns: string[], vals: any[]): number[] => {
+  let widthArr = clmns.map((v) => v.length);
+  for (const idx in widthArr) {
+    for (const val of vals) {
+      const v = String(val[idx]);
+      if (widthArr[idx] < v.length) {
+        widthArr[idx] = v.length;
+      }
+    }
+  }
+
+  return widthArr;
 };
 
 const convert = (dataSrc: any) => {
@@ -41,15 +90,22 @@ const convert = (dataSrc: any) => {
     console.error("input data is not array");
   }
 
+  // 列情報
   const clmns = extractColumns(dataSrc);
-  console.log(createTableHeaderString(clmns));
-
+  // 行情報
+  let rows: any[][] = [];
   for (const d of dataSrc) {
     let vals: any[] = [];
     for (const k of clmns) {
-      vals.push(d[k] || " ");
+      vals.push(d[k] || "");
     }
-    console.log(createRow(vals));
+    rows.push(vals);
+  }
+  // 列の幅情報を取得する
+  const widthArr = extractWidth(clmns, rows);
+  printTableHeader(clmns, widthArr);
+  for (const r of rows) {
+    printRow(r, widthArr);
   }
 };
 
